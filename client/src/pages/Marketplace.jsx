e14fbcf8a01401cf88ea7694485f4008584d0346
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-02-02 12:51:36 +0530

    change ui

--- client/src/pages/Marketplace.jsx
+++ client/src/pages/Marketplace.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { ArrowLeftIcon, FilterIcon } from 'lucide-react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Marketplace = () => {
+
+    const navigate = useNavigate()
+    const [showFilterPhone, setShowFilterPhone]=useState(false)
+
+
+
   return (
-    <div>
-        <h1>Marketplace page</h1>
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
+        <div className='flex items-center justify-between text-slate-500'>
+            <button onClick={()=>{navigate('/'); scrollTo(0,0)}} className='flex items-center gap-2 py-5'>
+                <ArrowLeftIcon className='size-4'/>
+                Back to Home
+
+            </button>
+            <button onClick={()=>{}}>
+                <FilterIcon className='size-4'/>
+                Filters
+
+            </button>
+
+        </div>
+        <div>
+
+        </div>
     </div>
   )
 }
