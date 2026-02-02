2ae67f4633d3677aad877840b825aef2ec7acc3b
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-02-02 13:25:37 +0530

    sidebars

--- client/src/components/FilterSidebar.jsx
+++ client/src/components/FilterSidebar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const FilterSidebar = () => {
+const FilterSidebar = ({showFilterPhone, setShowFilterPhone, filters}) => {
   return (
-    <div>FilterSidebar</div>
+    <div className='max-sm:inset-0 z-100 max-sm:h-screen max-sm:overflow-scroll bg-white rounded-lg shadow-sm border border-gray-200 h-fit sticky top-24 md:min-w-[300px]'>
+        
+
+    </div>
   )
 }
 
