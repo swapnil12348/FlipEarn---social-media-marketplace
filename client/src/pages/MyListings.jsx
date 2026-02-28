0237e491a3c8a6394de065bdbaf55e6e997d9f86
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-02-28 15:34:09 +0530

    stat

--- client/src/pages/MyListings.jsx
+++ client/src/pages/MyListings.jsx
@@ -1,4 +1,4 @@
-import { CheckCircle, DollarSign, Eye, Plus, TrendingUp } from 'lucide-react';
+import { ArrowDownCircleIcon, CheckCircle, CoinsIcon, DollarSign, Eye, Plus, TrendingUp, WalletIcon } from 'lucide-react';
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
@@ -33,6 +33,25 @@ const MyListings = () => {
         <StatCard title='Active Listings' value={activeListings} icon={<CheckCircle className='size-6 text-green-600' />}color='green'/>
         <StatCard title='Sold' value={soldListings} icon={<TrendingUp className='size-6 text-indigo-600'/>} color='indigo'/>
         <StatCard title='Total Value' value={`${currency}${totalValue.toLocaleString()}`} icon={<DollarSign className='size-6 text-yellow-600' />}color='yellow'/>
+      </div>
+      {/* balance section */}
+      <div className='flex flex-col sm:flex-row justify-between gap-4 xl:gap-20 p-6 mb-10 bg-white rounded-xl border border-gray-200'>
+        {[
+          {label: 'Earned', value: balance.earned, icon:WalletIcon},
+          {label: 'Withdrawn', value: balance.withdrawn, icon: ArrowDownCircleIcon},
+          {label: 'Available', value: balance.available, icon: CoinsIcon },
+        ].map((item, index)=>(
+          <div key={index} className='flex flex-1 items-center justify-between p-4 rounded-lg border border-gray-100 cursor-pointer'>
+            <div className='flex items-center gap-3'>
+              <item.icon className='text-gray-500 w-6 h-6'/>
+              <span className='font-medium text-gray-600'>{item.label}</span>
+            </div>
+            <span className='text-xl font-medium text-gray-700'>
+              {currency}
+              {item.value.toFixed(2)}
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   )
