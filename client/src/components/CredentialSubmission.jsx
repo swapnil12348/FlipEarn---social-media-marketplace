f432148bac283b904379ad20e55501e07c5dfdde
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-03-02 22:21:32 +0530

    css

--- client/src/components/CredentialSubmission.jsx
+++ client/src/components/CredentialSubmission.jsx
@@ -1,3 +1,4 @@
+import { X } from 'lucide-react'
 import React, { useState } from 'react'
 import toast from 'react-hot-toast'
 
@@ -22,7 +23,20 @@ const CredentialSubmission = ({onClose, listing}) => {
     }
 
   return (
-    <div>
+    <div className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-100 flex items-center justify-center sm:p-4 '>
+        <div className='bg-white sm:rounded-lg shadow-2xl w-full max-w-lg h-screen sm:h-[320px] flex flex-col'>
+            {/* header */}
+            <div>
+                <div>
+                    <h3>{listing?.title}</h3>
+                    <p>Adding Credentials for {listing?.username} on {listing?.platform}</p>
+                </div>
+                <button>
+                    <X className='w-5 h-5'/>
+                </button>
+            </div>
+
+        </div>
        
     </div>
   )
