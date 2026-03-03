b847db7b96346aa2f6b5fdee225eb00dae229fa0
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-03-03 13:00:20 +0530

    arrays

--- client/src/pages/ManageListing.jsx
+++ client/src/pages/ManageListing.jsx
@@ -11,7 +11,37 @@ const ManageListing = () => {
   const [loadingListing, setLoadingListing] = useState(false)
   const [isEditing, setIsEditing] = useState(false)
 
-  
+  const [formData, setFormData] = useState({
+
+    title: '',
+    platform: '',
+    username: '',
+    followers_count: '',
+    engagement_rate: '',
+    monthly_views: '',
+    niche: '',
+    price: '',
+    description: '',
+    verified: false,
+    monetized: false,
+    country: '',
+    age_range: '',
+    images: [],
+
+
+  })
+
+  const platforms = ['youtube', 'instagram', 'tiktok', 'facebook', 'twitter', 'linkedin', 'pinterest', 'snapchat', 'twitch', 'discord']
+
+   const niches = ['lifestyle', 'fitness', 'food', 'travel', 'tech', 'gaming', 'fashion', 'beauty', 'buisness', 'education', 'entertainment', 'music', 'art', 'sports', 'health', 'finance', 'other'];
+
+   const ageRanges = ['13-17 years', '18-24 years', '25-34 years', '35-44 years', '45-54 years', '55+ years', 'Mixed ages']
+
+   const handleInputChange = (field, value)=>{
+    setFormData((prev) => ({...prev, [field]: value}))
+
+   }
+
 
   return (
     <div>
