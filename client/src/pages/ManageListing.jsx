ba214dc47eead71b92e4802c8c6d7a4975cdfa22
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-03-03 12:48:19 +0530

    listing

--- client/src/pages/ManageListing.jsx
+++ client/src/pages/ManageListing.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const ManageListing = () => {
+
+  const {id} = useParams()
+  const navigate = useNavigate()
+  const {userListings, balance} = useSelector((state)=>state.listing)
+
   return (
     <div>
         <h1>ManageListing page</h1>
