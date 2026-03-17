ddfc2d88c9d4aa1e2337b594b63f9c778d7eb768
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-03-17 20:55:25 +0530

    server.ks

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from "express"
 import "dotenv/config"
 import cors from "cors"
+import {clerkMiddleware} from '@clerk/express'
 
 const app = express();
 
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 app.get("/", (req, res)=> res.send("Server is live!"))
 
