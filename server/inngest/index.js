808babbedbb9439d69edc5cb835c31e21ae9e7cc
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-03-19 15:13:52 +0530

    commit

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,37 @@
 import { Inngest } from "inngest";
+import prisma from "../configs/prisma.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "my-app" });
+export const inngest = new Inngest({ id: "profile-marketplace" });
+
+// INNgest function to save user data to a database
+
+const syncUserCreation = inngest.createFunction(
+    {id: "sync-user-from-clerk" },
+    {event: "clerk/user.created" },
+    async({event}) =>{
+        const {data} = event
+
+        //check if user already exists in the database
+        const user = await prisma.user.findFirst({
+            where: {id: data.id},
+           
+        })
+
+        if(user){
+            //Update user data if it exists
+
+            await prisma.user.update({
+                where: {id:data.id},
+                data:{
+                email: data?.email_addresses[0]?.email_address,
+                }
+            })
+        }
+    }
+)
+    
+
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [];
