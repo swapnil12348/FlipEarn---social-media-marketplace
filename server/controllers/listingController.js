329e2fc14e2a6c78ea26f291acc517b08d55968c
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-03-24 12:25:12 +0530

    controller

---
+++ server/controllers/listingController.js
@@ -0,0 +1,11 @@
+// Controller for adding listing to database
+
+export const addListing = async (req,res) => {
+    try {
+        const {userId} = await req.auth();
+        
+    } catch (error) {
+        
+    }
+    
+}
