83380bef7ad08f666e0c31ab1a5e5b3915437d1e
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-04-21 15:04:41 +0530

    commit

--- server/routes/listingRoutes.js
+++ server/routes/listingRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { addListing } from "../controllers/listingController.js";
+import { addListing, updateListing } from "../controllers/listingController.js";
 import { protect } from "../middlewares/authMiddleware.js";
+import upload from "../configs/multer.js";
 
 const listingRouter = express.Router();
 
-listingRouter.post('/',protect, addListing)
+listingRouter.post('/',upload.array("images", 5) ,protect, addListing)
+listingRouter.put('/',upload.array("images", 5), protect, updateListing)
 
 export default listingRouter
