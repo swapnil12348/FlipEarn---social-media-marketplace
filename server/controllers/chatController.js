b6f9de771a8b48437a75646ad6d6a13297949bde
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-04-21 18:02:30 +0530

    commit

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,2 +1,16 @@
 //Controller for getiing chat(creating if not exists)
 
+export const getChat = async (req, res)=>{
+    try {
+        const {userId} = await req.auth();
+        const {listingId, chatId} = req.body;
+
+        const listing = await prisma.listing.findUnique({
+            where: {id: listingId}
+        })
+
+        
+    } catch (error) {
+        
+    }
+}
