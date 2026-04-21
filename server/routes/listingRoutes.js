bccfd23fa08e4de6ee0d924d32198c1f0e582507
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-04-21 15:01:04 +0530

    commit

--- server/routes/listingRoutes.js
+++ server/routes/listingRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { addListing } from "../controllers/listingController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const listingRouter = express.Router();
 
-listingRouter.post('/', addListing)
+listingRouter.post('/',protect, addListing)
 
 export default listingRouter
