1627ff0d41d8d82221b060cb67a830553ee6257e
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-04-21 15:10:48 +0530

    controllllers

--- server/routes/listingRoutes.js
+++ server/routes/listingRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addListing, updateListing } from "../controllers/listingController.js";
+import { addCredential, addListing, deleteUserListing, getAllPublicListing, getAllUserListing, getAllUserOrders, markFeatured, toggleStatus, updateListing } from "../controllers/listingController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 import upload from "../configs/multer.js";
 
@@ -7,5 +7,13 @@ const listingRouter = express.Router();
 
 listingRouter.post('/',upload.array("images", 5) ,protect, addListing)
 listingRouter.put('/',upload.array("images", 5), protect, updateListing)
+listingRouter.get('/public', getAllPublicListing)
+listingRouter.get('/user', protect, getAllUserListing)
+listingRouter.put('/:id/status', protect, toggleStatus)
+listingRouter.delete('/:listingId', protect, deleteUserListing)
+listingRouter.post('/add-credential', protect, addCredential)
+listingRouter.put('/featured/:id', protect, markFeatured)
+listingRouter.get('/user-orders', protect, getAllUserOrders)
+
 
 export default listingRouter
