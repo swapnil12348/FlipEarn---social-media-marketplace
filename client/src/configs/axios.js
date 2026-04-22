3bcfc9dccdb58e68ae97759abb3ae7f38b816418
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-04-22 15:12:09 +0530

    commit

--- client/src/configs/axios.js
+++ client/src/configs/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
 
-    baseURL: import.meta.env.VITE_BASEURL || 'http://localhost:3000'
+    baseURL : import.meta.env.VITE_BASEURL || 'http://localhost:3000'
 })
 
 export default api
