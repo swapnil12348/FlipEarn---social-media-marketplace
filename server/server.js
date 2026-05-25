c17779f79358b399a1011f2acaf42733438e786d
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-05-25 14:20:38 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import chatRouter from "./routes/chatRoutes.js";
 
 const app = express();
 
-app.use(express.json())
+
 app.use(cors())
 app.use(clerkMiddleware())
 
@@ -17,7 +17,7 @@ app.get("/", (req, res)=> res.send("Server is live!"))
 
 app.use("/api/inngest", serve({client: inngest, functions}))
 
-
+app.use(express.json())
 app.use("/api/listing", listingRouter)
 app.use("/api/chat", chatRouter)
 
