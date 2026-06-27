faa77301a4db29a24f949c271d32bb090a5ac21b
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-06-27 10:35:18 +0530

    finally completed?

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -58,7 +58,7 @@ const App = () => {
         <Route path='/edit-listing/:id' element={<ManageListing/>}/>
         <Route path='/messages' element={<Messages/>}/>
         <Route path='/my-orders' element={<MyOrders/>}/>
-        <Route path='/loading' element={<Loading/>}/>
+        <Route path='/loading/:nextUrl' element={<Loading/>}/>
         <Route path='/admin' element={<Layout/>}>
         <Route index element={<Dashboard/>}/>
         <Route path='verify-credentials' element={<CredentialVerify/>}/>
--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Loader2Icon } from 'lucide-react';
+import React, { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Loading = () => {
+
+  const{nextUrl}= useParams();
+  const navigate = useNavigate()
+  useEffect(()=>{
+    if (nextUrl) {
+      setTimeout(()=>{
+        navigate('/' + nextUrl)
+      }, 6000)
+      
+    }
+
+  }, [])
   return (
-    <div>
-        <h1>Loading...</h1>
+    <div className='flex justify-center items-center h-[80vh]'>
+        <Loader2Icon className='animate-spin text-indigo-600 size-7'/>
     </div>
   )
 }
